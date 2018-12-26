cab4cfe45213c45820c0860e3d5152efbf1232ff
cheng33c <asglitc12@gmail.com>
2018-12-26 15:56:55 +0800

    first commit

--- src/symbols/Array.java
+++ src/symbols/Array.java
@@ -6,7 +6,10 @@ public class Array extends Type {
     public Type of;
     public int size = 1;
 
+    // Init Array
     public Array(int sz, Type p) {
+	// sz * p.width means Array's length(size)  multip type size(p.width)
+	// 
 	super("[]", Tag.INDEX, sz*p.width);
 	size = sz;
 	of = p;
